41bfd0883005abaa84d09b405304dbef9ebe9ffa
HajarElMannani <laso125@hotmail.com>
2025-05-06 08:00:23 -0700

    First commit

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Function insert_school'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    ''' function that inserts a new docE ument in a
+    collection based on kwargs'''
+    ids = mongo_collection.insert_one(kwargs)
+    return ids
