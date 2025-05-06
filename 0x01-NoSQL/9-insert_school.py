104bfb5f7c7ccda3f5796253a15135f3daa9799f
HajarElMannani <laso125@hotmail.com>
2025-05-06 08:02:39 -0700

    Change return statement

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,4 +6,4 @@ def insert_school(mongo_collection, **kwargs):
     ''' function that inserts a new docE ument in a
     collection based on kwargs'''
     ids = mongo_collection.insert_one(kwargs)
-    return ids
+    return ids.inserted_id
